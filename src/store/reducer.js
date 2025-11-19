4470eca8458b0984b04d278d32961d55ce63b61c
danglh1910elmk <danglh1910elmk@gmail.com>
2025-11-20 01:34:05 +0700

    first commit

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -20,15 +20,6 @@ export default function reducer(state = initialState, action) {
                 todos: [action.payload, ...state.todos],
             };
         case EDIT_TODO:
-            console.log({
-                ...state,
-                todos: state.todos.map((todo) =>
-                    todo.id === action.payload.id
-                        ? { ...todo, title: action.payload.title }
-                        : todo
-                ),
-            });
-
             return {
                 ...state,
                 todos: state.todos.map((todo) =>
